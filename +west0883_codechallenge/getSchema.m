72ac5ab425b4b4891bdf80d7f7596ab31586c27d
Sarah West <west0883@umn.edu>
2022-05-26 09:58:05 -0500

    Add code for averaging neuron stas.

--- +west0883_codechallenge/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'west0883_codechallenge', 'west0883_codechallenge');
-end
-obj = schemaObject;
-end
