571ce0ed11c1912ffa8bb4d04ccfdac4630726ce
Sarah West <west0883@umn.edu>
2022-05-26 09:57:01 -0500

    Subject table.

---
+++ +slwest382_codechallenge/Subject.m
@@ -0,0 +1,9 @@
+%{
+# Subject
+subject_name: varchar(30)                  # unique subject id, string up to
+                                           #  30 characters                                       
+---
+%}
+
+classdef Subject < dj.Manual
+end
