e8e168a809376bc42ea95dc0775ae7270cdc1df0
Sarah West <west0883@umn.edu>
2022-05-26 09:58:11 -0500

    Fix spike weighting issue.

--- +slwest382_codechallenge/NeuronComputations.m
+++ +slwest382_codechallenge/NeuronComputations.m
@@ -15,6 +15,9 @@ classdef NeuronComputations < dj.Computed
             % Fetch the spike-triggered averages & stds of the neuron
             [stas, stds] = fetchn(slwest382_codechallenge.SpikeTriggeredAverage & key, 'sta', 'std');
             
+            % Fetch the number of spikes per stim. 
+            spike_times = fetchn(slwest382_codechallenge.Recording & key, 'spike_times'); 
+
             % If empty. skip (weird, I think it should always give an NaN?)
             if isempty(stas) 
                 key.avg_sta = NaN;
@@ -28,10 +31,21 @@ classdef NeuronComputations < dj.Computed
                % Concatenate 
                stas_together = cat(3, stas{:});
                stds_together = cat(3, stds{:});
+               
+               % Weight each based on number of spikes.
+               spike_numbers = NaN(numel(spike_times),1);
+               for i = 1:numel(spike_times)
+                     spike_numbers(i) = numel(spike_times{i});
+                     stas_together(:,:, i) = stas_together(:,:,i) *spike_numbers(i);
+                     stds_together(:,:, i) = stds_together(:,:,i) *spike_numbers(i);
+               end 
 
-               % Take mean. 
-               key.avg_sta = mean(stas_together, 3, 'omitnan');
-               key.avg_std = std(stds_together, [], 3, 'omitnan');
+               % Take weighted mean. 
+               key.avg_sta = sum(stas_together,3) ./ sum(spike_numbers);
+               key.avg_std = sum(stds_together,3) ./ sum(spike_numbers);
+
+%                key.avg_sta = mean(stas_together, 3, 'omitnan');
+%                key.avg_std = mean(stds_together, 3, 'omitnan');
 
             end
             self.insert(key);
